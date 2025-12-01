80e6c08da94e15863fd5ff35dfbc1186206893f7
Jhonny515 <jonatascemporcentojesus@gmail.com>
2025-12-01 19:18:11 -0300

    About view

--- src/main/java/com/jhonny/App.java
+++ src/main/java/com/jhonny/App.java
@@ -9,6 +9,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 
 public class App extends Application {
+    private static Scene mainScene;
+
     @Override
     public void start(Stage primaryStage) {
         try {
@@ -18,13 +20,17 @@ public class App extends Application {
             scrollPane.setFitToWidth(true);
             scrollPane.setFitToHeight(true);
 
-            Scene mainScene = new Scene(scrollPane);
+            mainScene = new Scene(scrollPane);
             primaryStage.setScene(mainScene);
             primaryStage.setTitle("Sample JavaFX application");
             primaryStage.show();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static Scene getMainScene() {
+        return mainScene;
     }
 
     public static void main(String[] args) {
