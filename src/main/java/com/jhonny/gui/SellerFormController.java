315d3c9b8b71b305cb7306bde7d52d8a593ae7fc
Jhonny515 <jonatascemporcentojesus@gmail.com>
2026-01-26 19:09:28 -0300

    SellerForm

---
+++ src/main/java/com/jhonny/gui/SellerFormController.java
@@ -0,0 +1,127 @@
+package com.jhonny.gui;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+import com.jhonny.db.DbException;
+import com.jhonny.gui.listeners.DataChangeListener;
+import com.jhonny.gui.util.Alerts;
+import com.jhonny.gui.util.Constraints;
+import com.jhonny.gui.util.Utils;
+import com.jhonny.model.entity.Seller;
+import com.jhonny.model.exceptions.ValidationException;
+import com.jhonny.model.services.SellerService;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class SellerFormController implements Initializable {
+
+    private Seller entity;
+    private SellerService service;
+    @FXML
+    private TextField txtId;
+    @FXML
+    private TextField txtName;
+    @FXML
+    private Label labelErrorName;
+    private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
+
+    @FXML
+    private Button btSave;
+    @FXML
+    private Button btCancel;
+
+    public void setSeller(Seller entity) {
+        this.entity = entity;
+    }
+
+    public void setSellerService(SellerService service) {   
+        this.service = service;
+    }
+
+    public void subscribeDataChangeListener(DataChangeListener listener) {
+        dataChangeListeners.add(listener);
+    }
+
+    @FXML
+    public void onBtSaveAction(ActionEvent event) {
+        if (entity == null) {
+            throw new IllegalStateException("Entity was not set");
+        }
+        if (service == null) {
+            throw new IllegalStateException("Service was not set");
+        }
+        try {
+            entity = getFormData();
+            service.saveOrUpdate(entity);
+            notifyDataChangeListeners();
+            Utils.currentStage(event).close();
+        } catch (DbException e) {
+            Alerts.showAlert("Error", "Error saving object", e.getMessage(), AlertType.ERROR);
+        } catch (ValidationException e) {
+            setErrorMessages(e.getErrors());
+        }
+    }
+    @FXML
+    public void onBtCancelAction(ActionEvent event) {
+        Utils.currentStage(event).close();
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        initializeNodes();
+    }
+
+    private void initializeNodes() {
+        Constraints.setTextFieldInteger(txtId);
+        Constraints.setTextFieldMaxLength(txtName, 30);
+    }
+
+    public void updateFormData() {
+        if (entity == null) {
+            throw new IllegalStateException("Entity was not set");
+        }
+        txtId.setText(String.valueOf(entity.getId()));
+        txtName.setText(entity.getName());
+    }
+
+    private void notifyDataChangeListeners() {
+        for (DataChangeListener listener : dataChangeListeners) {
+            listener.onDataChanged();
+        }
+    }
+
+    public Seller getFormData() {
+        ValidationException exception = new ValidationException("Validation error");
+        if (txtName.getText() == null || txtName.getText().trim().equals("")) {
+            exception.addError("name", "Field can't be empty");
+        }
+
+        entity.setId(Utils.tryParseToInt(txtId.getText()));
+        entity.setName(txtName.getText());
+        
+        if (exception.getErrors().size() > 0) {
+            throw exception;
+        }
+
+        return entity;
+    }
+
+    private void setErrorMessages(Map<String, String> errors) {
+        Set<String> fields = errors.keySet();
+        if (fields.contains("name")) {
+            labelErrorName.setText(errors.get("name"));
+        }
+    }
+}
--- src/main/java/com/jhonny/gui/SellerListController.java
+++ src/main/java/com/jhonny/gui/SellerListController.java
@@ -63,7 +63,7 @@ public class SellerListController implements Initializable, DataChangeListener {
     public void onButtonNewAction(ActionEvent event) {
         Stage parentStage = Utils.currentStage(event);
         Seller obj = new Seller();
-        // createDialogForm(obj, "/gui/SellerForm.fxml", parentStage);
+        createDialogForm(obj, "/gui/SellerForm.fxml", parentStage);
     }
 
     @Override
@@ -99,29 +99,29 @@ public class SellerListController implements Initializable, DataChangeListener {
         tableViewSeller.prefHeightProperty().bind(stage.heightProperty().subtract(40));
     }
 
-    // private void createDialogForm(Seller entity, String absoluteName, Stage parentStage) {
-    //     try {
-    //         FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-    //         AnchorPane pane = loader.load();
+    private void createDialogForm(Seller entity, String absoluteName, Stage parentStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+            AnchorPane pane = loader.load();
 
-    //         SellerFormController controller = loader.getController();
-    //         controller.setSeller(entity);
-    //         controller.setSellerService(new SellerService());
-    //         controller.subscribeDataChangeListener(this);
-    //         controller.updateFormData();
+            SellerFormController controller = loader.getController();
+            controller.setSeller(entity);
+            controller.setSellerService(new SellerService());
+            controller.subscribeDataChangeListener(this);
+            controller.updateFormData();
 
-    //         Stage dialogStage = new Stage();
-    //         dialogStage.setTitle("Enter Seller Data");
-    //         dialogStage.setScene(new Scene(pane));
-    //         dialogStage.setResizable(false);
-    //         dialogStage.initOwner(parentStage);
-    //         dialogStage.initModality(Modality.WINDOW_MODAL);
-    //         dialogStage.showAndWait();
-    //     } catch (IOException e) {
-    //         Alerts.showAlert("Error", "Error loading view", e.getMessage(), AlertType.ERROR);
-    //         e.printStackTrace();
-    //     }
-    // }
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("Enter Seller Data");
+            dialogStage.setScene(new Scene(pane));
+            dialogStage.setResizable(false);
+            dialogStage.initOwner(parentStage);
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.showAndWait();
+        } catch (IOException e) {
+            Alerts.showAlert("Error", "Error loading view", e.getMessage(), AlertType.ERROR);
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public void onDataChanged() {
@@ -141,10 +141,9 @@ public class SellerListController implements Initializable, DataChangeListener {
                     return;
                 }
                 setGraphic(button);
-                // button.setOnAction(
-                        // event -> createDialogForm(
-                                // obj, "/gui/SellerForm.fxml", Utils.currentStage(event)));
-                                // null);
+                button.setOnAction(
+                        event -> createDialogForm(
+                                obj, "/gui/SellerForm.fxml", Utils.currentStage(event)));
             }
         });
     }
